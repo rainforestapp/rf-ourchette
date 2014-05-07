dc25be22fd054d77ed968cf3e27ee13fe7252ab4
Andrew Hooker <GeekOnCoffee@gmail.com>
2014-05-07 14:00:01 -0500

    Improve error message
    Identify what ENV are missing when raising an exception

--- lib/rainforest.rb
+++ lib/rainforest.rb
@@ -1,4 +1,6 @@
-raise 'missing at least one environment variable' unless ENV['FOURCHETTE_CLOUDFLARE_API_KEY'] && ENV['FOURCHETTE_CLOUDFLARE_EMAIL'] && ENV['FOURCHETTE_CLOUDFLARE_DOMAINS']
+missing_env = ['FOURCHETTE_CLOUDFLARE_API_KEY','FOURCHETTE_CLOUDFLARE_EMAIL', 'FOURCHETTE_CLOUDFLARE_DOMAINS'] - ENV.keys
+raise 'missing the following environment variable(s):' + missing_env.join(", ") if missing_env.any?
+
 
 module Rainforest
   ZONES = ENV['FOURCHETTE_CLOUDFLARE_DOMAINS'].to_s.split(',').map(&:strip)
