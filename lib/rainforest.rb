9ff84b05bd609e316170409256f9ffb438c5a4c4
Andrew Hooker <geekoncoffee@gmail.com>
2014-05-07 14:05:52 -0500

    Incorporate feedback from HoundCI

--- lib/rainforest.rb
+++ lib/rainforest.rb
@@ -1,6 +1,12 @@
-missing_env = ['FOURCHETTE_CLOUDFLARE_API_KEY','FOURCHETTE_CLOUDFLARE_EMAIL', 'FOURCHETTE_CLOUDFLARE_DOMAINS'] - ENV.keys
-raise 'missing the following environment variable(s):' + missing_env.join(", ") if missing_env.any?
+missing_env = %w{ 
+                  FOURCHETTE_CLOUDFLARE_API_KEY 
+                  FOURCHETTE_CLOUDFLARE_EMAIL 
+                  FOURCHETTE_CLOUDFLARE_DOMAINS
+                } - ENV.keys
 
+if missing_env.any? do
+  raise "missing the following environment variable(s): #{missing_env.join(", ")}"
+end
 
 module Rainforest
   ZONES = ENV['FOURCHETTE_CLOUDFLARE_DOMAINS'].to_s.split(',').map(&:strip)
