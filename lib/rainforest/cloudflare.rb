21da77d3fd1ce9b131d38e78d91fdc46122b137f
Jean-Philippe Boily <j@jipi.ca>
2014-11-19 11:52:28 -0500

    remove useless whitespaces

--- lib/rainforest/cloudflare.rb
+++ lib/rainforest/cloudflare.rb
@@ -1,13 +1,13 @@
 require 'cloudflare'
 class Rainforest::Cloudflare
   include Fourchette::Logger
-  
+
   def initialize(apps = [])
     @apps = apps
     @client = CloudFlare::connection(ENV['FOURCHETTE_CLOUDFLARE_API_KEY'], ENV['FOURCHETTE_CLOUDFLARE_EMAIL'])
     @zones = Rainforest::ZONES
   end
-  
+
   def create_subdomains(pull_request_id, heroku_app_url)
     @apps.each do |app|
       record_name = "#{app}-#{pull_request_id}"
@@ -22,7 +22,7 @@ class Rainforest::Cloudflare
       end
     end
   end
-  
+
   def delete_subdomains(pull_request_id)
     raise 'invalid pull_request_id' unless pull_request_id.to_i > 0
     @apps.each do |app|
@@ -39,11 +39,11 @@ class Rainforest::Cloudflare
       end
     end
   end
-  
+
   def all_dns_records(zone)
     @client.rec_load_all(zone)['response']['recs']['objs']
   end
-  
+
   def dns_record_id(record_name, zone)
     domain_to_match = "#{record_name}.#{zone}"
     all_dns_records(zone).each do |rec|
